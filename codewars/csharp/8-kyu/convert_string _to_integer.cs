af57daeb7a34cf9d0d0af66a6808340dbe672cc6
lodashreload <icarusjapan@gmail.com>
2021-11-20 01:26:38 -0500

    converting int to string

---
+++ codewars/csharp/8-kyu/convert_string _to_integer.cs
@@ -0,0 +1,16 @@
+using System;
+public class Kata
+{
+    public static int StringToNumber(String str)
+    {
+        int number = 1000;
+        string s = Convert.ToString(number);
+        console.WriteLine(s);
+    }
+}
+//P Convert a int into a sting number
+//E 1234 --> "1234"
+//D int string ToString
+//A for any int add
+// convert to string 
+//C
