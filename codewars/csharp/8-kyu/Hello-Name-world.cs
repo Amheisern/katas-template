69d294f4479ee6629d9c2a32e470e1349c17694b
lodashreload <icarusjapan@gmail.com>
2021-11-24 12:37:03 -0500

    MORE KATA WORK TO FAIL AT

---
+++ codewars/csharp/8-kyu/Hello-Name-world.cs
@@ -0,0 +1,13 @@
+using System;
+
+public static class Kata
+{
+
+    public static string Hello(string name)
+    {
+        Console.WriteLine("What is your name?");
+        Console.ReadLine();
+        // If (name == ()
+        //  return "Hello, CodeWarrior!";
+    }
+}
