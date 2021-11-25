dd0336415cddd9bc70f2d58fa303b10298da2fa3
lodashreload <icarusjapan@gmail.com>
2021-11-25 18:34:34 -0500

    Take a step back to take two steps forward

--- codewars/csharp/8-kyu/Hello-Name-world.cs
+++ codewars/csharp/8-kyu/Hello-Name-world.cs
@@ -1,13 +1,36 @@
+// using System;
+
+// public static class Kata
+// {
+
+//     public static string Hello(string name)
+//     {
+//         Console.WriteLine("What is your name?");
+//         Console.ReadLine(name);
+
+//         // If (name == ()
+//         if (name == "")
+//         {
+//             return "Hello, World!";
+//         }
+//         return "Hello, name is " + name;
+//         //  return "Hello, CodeWarrior!";
+//     }
+// }
 using System;
 
 public static class Kata
 {
-
+    public static string Hello()
+    {
+        return Hello("");
+    }
     public static string Hello(string name)
     {
-        Console.WriteLine("What is your name?");
-        Console.ReadLine();
-        // If (name == ()
-        //  return "Hello, CodeWarrior!";
+        string nam = "";
+        if (name == "") nam = "World";
+        else nam = Char.ToUpperInvariant(name[0]) + name.Substring(1).ToLower();
+
+        return "Hello, " + nam + "!";
     }
 }
